1a46891f5037653091dbbb0c74c34e0141aefedf
Marta Vasconcelos <mvasconcelos@singlestore.com>
2024-07-24 15:49:01 +0100

    Initial commit

---
+++ app/eleganceServerClient.ts
@@ -0,0 +1,34 @@
+import { createEleganceServerClient } from "@singlestore/elegance-sdk/server";
+
+let cert;
+
+fetch("https://portal.singlestore.com/static/ca/singlestore_bundle.pem")
+  .then(function (response) {
+    response.text().then(function (text) {
+      cert = text;
+    });
+  })
+
+let extraSettings;
+if (process.env.TIER === "shared") {
+  extraSettings = {
+    ssl: {
+      cert
+    }
+  }
+}
+export const eleganceServerClient = createEleganceServerClient("mysql", {
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+    ...extraSettings
+  },
+  ai: {
+    openai: {
+      apiKey: process.env.OPENAI_API_KEY
+    }
+  }
+});
